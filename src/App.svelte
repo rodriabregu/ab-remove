eff3d64ef9c94c1bbcb21c82f20621e703cb8978
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-03-13 23:26:54 +1000

    feat/main update

--- src/App.svelte
+++ src/App.svelte
@@ -5,28 +5,20 @@
   import { ImageStatus } from '../types.d';
 </script>
 
-
-<main class="flex flex-col w-full items-center gap-80">
-  <header class="text-4xl font-semibold">
+<main class="flex flex-col w-full items-center gap-10 max-w-xl mx-auto">
+  <header class="text-4xl font-semibold mt-4">
     <h1>Remove the <span class="text-purple-700">Background</span></h1>
   </header>
-  
-  {#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING }
+
+  {#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING}
     <Upload />
   {/if}
 
   {#if $imageStatus === ImageStatus.DONE}
     <Edit />
   {/if}
-
-  <!-- {#if $imageStatus === ImageStatus.ERROR}
-    <div class="flex flex-col items-center gap-4">
-      <img src={svelteLogo} alt="Svelte Logo" class="w-40" />
-      <h2 class="text-2xl font-semibold">Something went wrong</h2>
-      <p class="text-center">Please try again later</p>
-    </div>
-  {/if} -->
-
-  <footer>Footer</footer>
 </main>
 
+<footer class="absolute bottom-0 w-full text-center text-gray-500 text-sm py-4">
+  Footer
+</footer>
