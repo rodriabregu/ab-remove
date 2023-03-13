2729e1a78a78b196c60db8d4fe3813c730e79e85
Rodrigo Alcino <82095699+rodriabregu@users.noreply.github.com>
2023-03-13 23:26:43 +1000

    feat/main components

--- src/components/Edit.svelte
+++ src/components/Edit.svelte
@@ -1,9 +1,43 @@
 <script lang="ts">
-  import 'two-up-element'
+  import 'two-up-element';
   import { originalImage, processedImage } from '../store';
+
+  let processingImage = true;
+  let tries = 0;
+  let intervalId: number;
+
+  $: {
+    if (processingImage) {
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        tries++;
+        const img = new Image();
+        img.src = $processedImage;
+        img.onload = () => {
+          processingImage = false;
+          clearInterval(intervalId);
+        };
+      }, 750);
+    }
+  }
 </script>
 
 <two-up>
   <img src={$originalImage} alt="Original" />
-  <img src={$processedImage} alt="Edited" />
+
+  {#if processingImage}
+    <div class="flex flex-col items-center gap-4">
+      <h2 class="text-2xl font-semibold">Processing image</h2>
+      <p class="text-center">Please wait</p>
+    </div>
+  {:else}
+    <img src={$processedImage} alt="Processed" />
+  {/if}
 </two-up>
+
+<a
+  download
+  href={$processedImage}
+  class="bg-violet-500 text-white py-2 px-2 rounded-md text-xl font-bold"
+  >Download image without background</a
+>
--- src/components/Upload.svelte
+++ src/components/Upload.svelte
@@ -1,56 +1,80 @@
 <script lang="ts">
   import Dropzone from 'dropzone';
+  import { Cloudinary } from '@cloudinary/url-gen';
+  import { backgroundRemoval } from '@cloudinary/url-gen/actions/effect';
   import 'dropzone/dist/dropzone.css';
+
   import { onMount } from 'svelte';
   import { ImageStatus } from '../../types.d';
-  import { imageStatus, originalImage } from '../store';
+  import { imageStatus, originalImage, processedImage } from '../store';
 
-  onMount(
-    () => {
-      const dropzone = new Dropzone('#dropzone', {
-        // url: 'https://api.cloudinary.com/v1_1/rodriab/image/upload',
-        maxFiles: 1,
-        acceptedFiles: 'image/*',
-        uploadMultiple: false,
-        addRemoveLinks: true,
-        dictRemoveFile: 'Remove',
-        dictDefaultMessage: 'Drop files here to upload',
-        dictFallbackMessage: 'Your browser does not support drag\'n\'drop file uploads.',
-        dictFileTooBig: 'File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.',
-        dictInvalidFileType: 'You can\'t upload files of this type.',
-        dictResponseError: 'Server responded with {{statusCode}} code.',
-        dictCancelUpload: 'Cancel upload',
-        dictCancelUploadConfirmation: 'Are you sure you want to cancel this upload?',
-        dictRemoveFileConfirmation: null,
-        dictMaxFilesExceeded: 'You can not upload any more files.',
-      });
+  const cloudinary = new Cloudinary({
+    cloud: {
+      cloudName: 'rodriab',
+    },
+    url: {
+      secure: true,
+    },
+  });
 
-      dropzone.on('sending', (file, xhr, formData) => {
-        imageStatus.set(ImageStatus.UPLOADING);
-        formData.append('upload_preset', 'llw79me8');
-        formData.append('timestamp', (Date.now() / 1000));
-        formData.append('api_key', '488249629195227');
-      });
+  onMount(() => {
+    const dropzone = new Dropzone('#dropzone', {
+      maxFiles: 1,
+      acceptedFiles: 'image/*',
+      uploadMultiple: false,
+      addRemoveLinks: true,
+      dictRemoveFile: 'Remove',
+      dictDefaultMessage: 'Drop files here to upload',
+      dictFallbackMessage:
+        "Your browser does not support drag'n'drop file uploads.",
+      dictFileTooBig:
+        'File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.',
+      dictInvalidFileType: "You can't upload files of this type.",
+      dictResponseError: 'Server responded with {{statusCode}} code.',
+      dictCancelUpload: 'Cancel upload',
+      dictCancelUploadConfirmation:
+        'Are you sure you want to cancel this upload?',
+      dictRemoveFileConfirmation: null,
+      dictMaxFilesExceeded: 'You can not upload any more files.',
+    });
 
-      dropzone.on('success', (file, response) => {
-        const { secure_url: url } = response;
+    dropzone.on('sending', (file, xhr, formData) => {
+      imageStatus.set(ImageStatus.UPLOADING);
+      formData.append('upload_preset', 'llw79me8');
+      formData.append('timestamp', Date.now() / 1000);
+      formData.append('api_key', '488249629195227');
+    });
 
-        imageStatus.set(ImageStatus.DONE);
-        originalImage.set(url);
-        console.log(response);
-      });
+    dropzone.on('success', (file, response) => {
+      const { public_id: publicId, secure_url: url } = response;
 
-      dropzone.on('error', (file, response) => {
-        imageStatus.set(ImageStatus.ERROR);
-        console.log(response);
-      });
-      });
+      const imgWithoutBg = cloudinary
+        .image(publicId)
+        .effect(backgroundRemoval())
+        .toURL();
+
+      imageStatus.set(ImageStatus.DONE);
+      originalImage.set(url);
+      processedImage.set(imgWithoutBg);
+    });
+
+    dropzone.on('error', (file, response) => {
+      imageStatus.set(ImageStatus.ERROR);
+    });
+  });
 </script>
 
-<form id="dropzone" action="https://api.cloudinary.com/v1_1/rodriab/image/upload" class="aspect-video rounded-md border-dashed border-1 border-gray-300 shadow-md flex flex-col items-center p-4">
+<form
+  id="dropzone"
+  action="https://api.cloudinary.com/v1_1/rodriab/image/upload"
+  class="aspect-video rounded-md border-dashed border-4 border-gray-300 shadow-md flex flex-col items-center py-16 px-32 cursor-pointer"
+>
   {#if $imageStatus === ImageStatus.READY}
-  <button class="pointer-events-none bg-violet-500 text-white py-4 px-2 rounded-md text-xl font-bold">Uplaoad files</button>
-  <strong class="text-md mt-4">or drop a file</strong>
+    <button
+      class="pointer-events-none bg-violet-500 text-white py-4 px-2 rounded-md text-xl font-bold"
+      >Upload files</button
+    >
+    <strong class="text-md mt-4">or drop a file</strong>
   {/if}
 
   {#if $imageStatus === ImageStatus.UPLOADING}
@@ -58,6 +82,6 @@
   {/if}
 
   {#if $imageStatus === ImageStatus.ERROR}
-    <strong class="text-md">Error</strong>
+    <strong class="text-md">Error, try again in few minutes.</strong>
   {/if}
 </form>
